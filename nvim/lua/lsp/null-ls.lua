1ff1ecac847ee26d2a6c88e4fcfc08350849a7a0
snafudexter <prabh@live.in>
2022-03-27 21:30:28 +0530

    removed extra files

--- nvim/lua/lsp/null-ls.lua
+++ nvim/lua/lsp/null-ls.lua
@@ -3,16 +3,65 @@ if not null_ls_status_ok then
 	return
 end
 
+local null_ls_helpers_status_ok, null_helpers = pcall(require, "null-ls.helpers")
+if not null_ls_helpers_status_ok then
+	return
+end
+
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
 local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
+
+-- AWS CFN lint
+local cfn_lint = {
+	method = null_ls.methods.DIAGNOSTICS,
+	filetypes = { "yaml" },
+	generator = null_helpers.generator_factory({
+		command = "cfn-lint",
+		to_stdin = true,
+		to_stderr = true,
+		args = { "--format", "parseable", "-" },
+		format = "line",
+		check_exit_code = function(code)
+			return code == 0 or code == 255
+		end,
+		on_output = function(line, params)
+			local row, col, end_row, end_col, code, message = line:match(":(%d+):(%d+):(%d+):(%d+):(.*):(.*)")
+			local severity = null_helpers.diagnostics.severities["error"]
+
+			if message == nil then
+				return nil
+			end
+
+			if vim.startswith(code, "E") then
+				severity = null_helpers.diagnostics.severities["error"]
+			elseif vim.startswith(code, "W") then
+				severity = null_helpers.diagnostics.severities["warning"]
+			else
+				severity = null_helpers.diagnostics.severities["information"]
+			end
+
+			return {
+				message = message,
+				code = code,
+				row = row,
+				col = col,
+				end_col = end_col,
+				end_row = end_row,
+				severity = severity,
+				source = "cfn-lint",
+			}
+		end,
+	}),
+}
 
 -- https://github.com/prettier-solidity/prettier-plugin-solidity
 -- npm install --save-dev prettier prettier-plugin-solidity
 null_ls.setup({
 	debug = false,
 	sources = {
+		cfn_lint,
 		formatting.prettier.with({
 			extra_filetypes = { "toml", "solidity" },
 			extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
